6d62bca4b0ac13c90d0e5da5f9575a542eea12b1
BinotaLiu <binota@binota.org>
2018-05-23 22:11:00 +0800

    add form validation

--- app/Http/Controllers/DonationsController.php
+++ app/Http/Controllers/DonationsController.php
@@ -11,8 +11,15 @@ class DonationsController extends Controller
 {
     public function store(Request $request)
     {
-        // @todo: form validation
-        $this->validate($request, []);
+        $this->validate($request, [
+            'profile.name' => 'required',
+            'profile.phone' => 'required',
+            'profile.email' => 'required|email',
+            'profile.address' => 'required',
+            'payment.type' => 'required|in:' . implode(',', DonationType::values()),
+            'payment.amount' => 'required|integer',
+            'payment.custom_amount' => 'required_if:amount,0',
+        ]);
 
         $profile = $request->input('profile');
         $payment = $request->input('payment');
