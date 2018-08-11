8b7699e34a1631b3a36b1b2f9aa4491df64a8b56
BinotaLiu <binota@binota.org>
2018-08-11 14:28:13 +0800

    disable csrf for callback page

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,7 @@ class VerifyCsrfToken extends Middleware
      * @var array
      */
     protected $except = [
-        //
+        '/donations/*/first-callback',
+        '/donations/*/period-callback',
     ];
 }
