ba3245623cebed88c62689b7974aa8fd067b35f9
BinotaLIU <me@binota.org>
2020-08-09 17:13:03 +0800

    fix(components): card height

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,7 +1,7 @@
-<div {{ $attributes->merge(['class' => "bg-{$color} rounded shadow"]) }}>
-    <div class="px-4 py-2 border border bg-{{ $titleColor }}-100 rounded-t">{{ $title }}</div>
+<div {{ $attributes->merge(['class' => "flex flex-col justify-start bg-{$color} rounded shadow"]) }}>
+    <div class="px-4 py-2 border border bg-{{ $titleColor }}-100 rounded-t">{{ $title ?? '' }}</div>
 
-    <div class="p-4 border border-t-0 rounded-b">
+    <div class="flex-grow p-4 border border-t-0 rounded-b">
         {{ $slot }}
     </div>
 </div>
