9832ccfba9034b9156862220fd09051cb85c29e5
BinotaLIU <me@binota.org>
2020-08-09 21:56:50 +0800

    fix: login redirect

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Route;
 
 class RouteServiceProvider extends ServiceProvider
 {
-    public const HOME = 'home';
+    public const HOME = 'manage';
 
     /**
      * This namespace is applied to your controller routes.
