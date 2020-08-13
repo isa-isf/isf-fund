e4e53e74556d1a619b316a3fe138e5457bf5e0e4
BinotaLIU <me@binota.org>
2020-08-14 05:52:35 +0800

    fix: default to database record

--- app/Models/LoginLog.php
+++ app/Models/LoginLog.php
@@ -52,7 +52,7 @@ class LoginLog extends Model
     static public function createFromRequest(Request $request, ?User $user, LoginResult $result)
     {
         $instance = new self;
-        $instance->email = $request->input('email');
+        $instance->email = $request->input('email', $user->email);
         $instance->user_id = optional($user)->id;
         $instance->ip_address = $request->ip();
         $instance->user_agent = $request->userAgent();
