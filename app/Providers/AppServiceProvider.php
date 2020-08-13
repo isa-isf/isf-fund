d38919dac3ade06ef793214b6d3ac2bbd3016e2a
BinotaLIU <me@binota.org>
2020-08-14 01:54:05 +0800

    feat: use CarbonImmutable and add "when" macro

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,8 +4,11 @@ namespace App\Providers;
 
 use Auth0\Login\Contract\Auth0UserRepository as Auth0Contract;
 use Auth0\Login\Repository\Auth0UserRepository;
+use Carbon\Carbon;
+use Carbon\CarbonImmutable;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\Date;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -18,6 +21,11 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Paginator::useTailwind();
+
+        Date::use(CarbonImmutable::class);
+        Date::macro('when', function (bool $condition, callable $callback) {
+            return $condition ? $callback($this) : $this;
+        });
     }
 
     /**
